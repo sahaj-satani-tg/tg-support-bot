e881a5b113b9b04dfb80a90c9853d698b09c3d86
sahaj <sahaj.satani@testgrid.io>
2025-09-18 17:37:38 +0530

    port-config

--- app_milvus.py
+++ app_milvus.py
@@ -321,4 +321,5 @@ def process_feedback():
     return jsonify(reply=response_feedback), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=10000,debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
