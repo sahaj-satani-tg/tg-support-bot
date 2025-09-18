a30b1009cffaae7cb85f22b0a92bdf7c18f9018e
sahaj <sahaj.satani@testgrid.io>
2025-09-18 16:54:05 +0530

    change to default port

--- app_milvus.py
+++ app_milvus.py
@@ -321,4 +321,4 @@ def process_feedback():
     return jsonify(reply=response_feedback), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0",port=10000,debug=True)
